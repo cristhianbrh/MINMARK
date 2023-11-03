b06527afe59a6d547583d74c8e23f540ef555a37
cristhianbrh <cristhianalexand@gmail.com>
2023-11-03 16:08:23 -0500

    Nav 2.0

--- home/routes.js
+++ home/routes.js
@@ -1,25 +1,45 @@
 const routes = {
     user: "section_user",
+    mantenedor: "section_mantenedor",
     ventas: "section_ventas",
 }
-const sections = document.querySelectorAll('.sectionCamb'); 
-const nav_options = document.querySelectorAll('.nav_options'); 
+const sections = document.querySelectorAll('.sectionCamb');
+const nav_options = document.querySelectorAll('.nav_options');
 
-function goNewSection(rt){
+function goNewSection(rt) {
     let actual = nav_options[0];
-    if(event){
+    if (event) {
         actual = event.target;
     }
-    const section = document.getElementById(routes[rt]);
-    sections.forEach(sect => {
-        sect.style.display = "none";
-    });
-    
-    nav_options.forEach(option=>{
-        option.style.background = "transparent";
-    })
-    section.style.display = "block";
-    actual.style.background = "#ffaa";
-    
+
 }
-goNewSection('user')
+
+
+
+
+function viewChildrenNav(notIsParent = false, rt) {
+    let actual = nav_options[0];
+    if (event) {
+        actual = event.target;
+
+    }
+
+
+    if (!notIsParent) {
+        // actual.style.background = "red"
+        const childrens = actual.parentNode.childNodes[3];
+        childrens.style.display = (childrens.style.display == "none") ? "flex" : "none";
+    } else {
+        const section = document.getElementById(routes[rt]);
+        sections.forEach(sect => {
+            sect.style.display = "none";
+        });
+
+        nav_options.forEach(option => {
+            option.style.background = "transparent";
+        })
+        section.style.display = "block";
+        actual.style.background = "#ffaa";
+    }
+}
+// goNewSection('user')
